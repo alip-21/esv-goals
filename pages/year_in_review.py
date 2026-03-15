5485eb3ef3cfcbca226d01dab22dea6b5026fa62
alip-21 <agp_squash@hotmail.co.uk>
2026-03-15 20:43:06 +0000

    Update year_in_review.py

--- pages/year_in_review.py
+++ pages/year_in_review.py
@@ -61,5 +61,5 @@ st.markdown("")
 
 # 7. The "Hall of Shame" Chart
 st.subheader("Shots")
-shot_chart_data = (filtered_df.groupby("Person")["Shot"].sum().sort_values(ascending=False))
+shot_chart_data = (filtered_df.groupby("Person")["Shot"].sum().reset_index(name="Total Shots").sort_values(by="Total Shots",ascending=False))
 st.bar_chart(shot_chart_data)
