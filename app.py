8094f7d6b31ea3ac243a9683bd8d39ccc98522ce
alip-21 <agp_squash@hotmail.co.uk>
2026-03-15 19:54:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ st.set_page_config(layout="wide")
 # Define the pages
 home_page = st.Page("pages/home.py", title="Home", icon="🏠", default=True)
 year_in_review_page = st.Page("pages/year_in_review.py", title="Year in Review", icon="🗓️")
+database_page = st.page("pages/database.py", title="Goals Database", icon="💾")
 
 # Create navigation
 pg = st.navigation(
