bac08c5a188bc28c968a55d3ddf4b4a682545a63
alip-21 <agp_squash@hotmail.co.uk>
2026-03-23 20:58:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 import streamlit as st
 import pandas as pd
 
-def check_password():
+# def check_password():
 
-    def password_entered():
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
+#     def password_entered():
+#         if st.session_state["password"] == st.secrets["password"]:
+#             st.session_state["password_correct"] = True
+#             del st.session_state["password"]  # don't store password
+#         else:
+#             st.session_state["password_correct"] = False
 
-    if "password_correct" not in st.session_state:
-        st.text_input(
-            "Enter Password to Access ESV Goals", 
-            type="password", 
-            on_change=password_entered, 
-            key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        st.text_input(
-            "Enter Password to Access ESV Goals", 
-            type="password", 
-            on_change=password_entered, 
-            key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        return True
+#     if "password_correct" not in st.session_state:
+#         st.text_input(
+#             "Enter Password to Access ESV Goals", 
+#             type="password", 
+#             on_change=password_entered, 
+#             key="password"
+#         )
+#         return False
+#     elif not st.session_state["password_correct"]:
+#         st.text_input(
+#             "Enter Password to Access ESV Goals", 
+#             type="password", 
+#             on_change=password_entered, 
+#             key="password"
+#         )
+#         st.error("😕 Password incorrect")
+#         return False
+#     else:
+#         return True
 
-if check_password():
+# if check_password():
 
     # Page configuration
     st.set_page_config(layout="wide")
