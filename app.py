3acb33dd7547017d34610e903c754f76fc1b1baa
alip-21 <agp_squash@hotmail.co.uk>
2026-03-23 17:27:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ st.set_page_config(layout="wide")
 # Define the pages
 home_page = st.Page("pages/home.py", title="Home", icon="🏠", default=True)
 year_in_review_page = st.Page("pages/year_in_review.py", title="Year in Review", icon="🗓️")
+all_time_stats_page = st.Page("pages/all_time_stats.py", title="All Time Stats", icon="📈")
 database_page = st.Page("pages/database.py", title="Goals Database", icon="💾")
 
 # Create navigation
 pg = st.navigation(
-    {"Menu":[home_page, year_in_review_page, database_page]},
+    {"Menu":[home_page, year_in_review_page, all_time_stats_page, database_page]},
     position="top"
 )
 pg.run()
