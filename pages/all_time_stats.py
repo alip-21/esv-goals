96000a8e56e4617cdad472f1733caacc64056eb8
alip-21 <agp_squash@hotmail.co.uk>
2026-03-23 21:06:41 +0000

    Update all_time_stats.py

--- pages/all_time_stats.py
+++ pages/all_time_stats.py
@@ -175,7 +175,7 @@ selection = alt.selection_point(fields=[dimension_trend], bind='legend')
 
 trend_metric_map = {
     "Success Rate": {"col": "Success_Rate", "title": "Success Rate", "fmt": "%", "tip": ".0%"},
-    "Total Goals": {"col": "Total_Completed", "title": "Goals Met", "fmt": "d", "tip": "g"},
+    "Total Goals": {"col": "Total_Goals", "title": "Total Goals Set", "fmt": "d", "tip": "d"},
     "Total Shots": {"col": "Total_Shots", "title": "Total Shots", "fmt": "d", "tip": "g"}
 }
 m_info = trend_metric_map[metric_trend]
