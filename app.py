8a5066eb9ccab142fca65e5a36ecb6200089df6f
alip-21 <agp_squash@hotmail.co.uk>
2026-03-23 20:51:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def check_password():
 if check_password():
 
     # Page configuration
-    st.set_page_config(layout="centered")
+    st.set_page_config(layout="wide")
 
     # Define the pages
     home_page = st.Page("pages/home.py", title="Home", icon="🏠", default=True)
